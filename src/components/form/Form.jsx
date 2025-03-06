e401ffef64f7bdc2cef689075ea10f633bac38d2
ProstoLama27 <mankaegor25@gmail.com>
2025-03-06 17:07:52 +0500

    сделал запись задачь в список

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,12 +1,18 @@
+import { useContext } from "react";
 import styles from "./Form.module.scss"
+import { TaskContext } from "../../context/taskContext";
 const Form = () => {
+
+    const {newTask,setNewTask,addTask} = useContext(TaskContext)
+
     return ( 
         <div className={styles.form}>
             <form action="" className={styles.form__container}>
-                <input type="text" placeholder="Введите задачу"/>
-                <button>Записать</button>
+                <input type="text" placeholder="Введите задачу" value={newTask}
+                onChange={(e)=>{setNewTask(e.target.value)}}/>
+                
             </form>
-            
+            <button onClick={addTask}>Записать</button>
         </div>
      );
 }
